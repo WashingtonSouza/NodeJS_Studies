ad2bc9bae767eb9cedfd3a9ab5d9a04b8fc49375
washington <thewashington.souza@gmail.com>
2021-01-06 23:59:53 -0300

    feat: add postgress sql file and postgres test

--- mult-db/src/db/strategies/postgres.js
+++ mult-db/src/db/strategies/postgres.js
@@ -1,8 +1,60 @@
 const ICrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
+const { user, password } = require('./../../config')
 
 class PostgresDB extends ICrud {
   constructor() {
     super()
+    this._driver = null
+    this._heroes = null
+    this._connect()
+  }
+
+  async isConnected() {
+    try {
+      await this._driver.authenticate()
+    } catch (error) {
+      console.log('Fail!! ', error)
+      return false
+    }
+  }
+
+  async defineModel() {
+    const Heroes = this._driver.define('heroes', {
+      id: {
+        type: Sequelize.INTEGER,
+        require: true,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        require: true
+      },
+      power: {
+        type: Sequelize.STRING,
+        require: true
+      }
+    }, {
+      tableName: 'TB_HEROES',
+      freezeTableName: false,
+      timestamps: false
+    })
+
+    await Heroes.sync()
+  }
+
+  _connect() {
+    this._driver = new Sequelize(
+      'heroes',
+      user,
+      password, {
+      host: 'localhost',
+      dialect: 'postgres',
+      quoteIdentifiers: false,
+      operatorAliases: false
+    }
+    )
   }
 
   create(item) {
